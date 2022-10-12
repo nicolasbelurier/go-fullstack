e6f30b6fc5e3cde7ea29b9442aa86de6b13e2307
Enckka <mail.nicolas.belurier@gmail.com>
2022-10-12 18:15:41 +0200

    Ajout de middleware auth et multer

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const stuffCtrl = require ('../controllers/stuff')
 
-router.post('/', stuffCtrl.createThing);
-router.put('/:id', stuffCtrl.modifyThing);
-router.delete('/:id', stuffCtrl.deleteThing);
-router.get('/', stuffCtrl.getThing);
-router.get('/:id', stuffCtrl.getOneThing);
+const auth = require('../middleware/auth');
+
+const stuffCtrl = require('../controllers/stuff');
+
+router.get('/', auth, stuffCtrl.getThing);
+router.post('/', auth, stuffCtrl.createThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
