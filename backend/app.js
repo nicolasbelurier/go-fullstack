48a108b9288f03491f256438b43711c94db58c3b
Enckka <mail.nicolas.belurier@gmail.com>
2022-10-12 18:42:56 +0200

    ajout "Path" afin de pouvoir upload des images

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
 
+const path = require('path');
+
 mongoose.connect('mongodb+srv://admin:admin@cluster0.0sb1dfi.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -23,5 +25,6 @@ app.use((req, res, next) => {
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,14 +1,19 @@
 const Thing = require('../models/Thing')
 
 exports.createThing = (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-      ...req.body
-    });
-    thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré.'}))
-    .catch(error => res.status(400).json({ error }));
-  }
+  const thingObject = JSON.parse(req.body.thing);
+  delete thingObject._id;
+  delete thingObject._userId;
+  const thing = new Thing({
+      ...thingObject,
+      userId: req.auth.userId,
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  });
+
+  thing.save()
+  .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
+  .catch(error => { res.status(400).json( { error })})
+};
 
 exports.modifyThing = (req, res, next) => {
     Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
@@ -22,7 +27,7 @@ exports.deleteThing = (req, res, next) => {
       .catch(error => res.status(400).json({ error }));
   }
 
-exports.getThing = (req, res, next) => {
+exports.getAllThings = (req, res, next) => {
     Thing.find()
     .then(things => res.status(200).json(things))
     .catch(error => res.status(400).json({ error }));
