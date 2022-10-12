95b3255caf7477ca37f9248ceccffbd6b658f1b3
Enckka <mail.nicolas.belurier@gmail.com>
2022-10-12 14:53:07 +0200

    Ajout de controller et routes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const stuffRoutes = require('./routes/stuff');
+
 mongoose.connect('mongodb+srv://admin:admin@cluster0.0sb1dfi.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -18,33 +20,6 @@ app.use((req, res, next) => {
     next();
   });
 
-app.post('/api/stuff/', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Objet créé.'
-  });
-});
-
-app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
